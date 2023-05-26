6b21504db4ca29fd544882229b65899471772b5b
Suyog <np03cs4a220467@heraldcollege.edu.np>
2023-05-26 22:47:34 +0545

    second commit

--- django_chatbot/django_chatbot/urls.py
+++ django_chatbot/django_chatbot/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('chatbot.urls'))
 ]
+    # when user tries to go into home page it go into chatbot.urls where
+    # where html is opned
