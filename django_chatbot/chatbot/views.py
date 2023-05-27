25e891980efe5ab0c8281739ec0738f23e9d5e2c
Suyog <np03cs4a220467@heraldcollege.edu.np>
2023-05-27 10:27:55 +0545

    incomplete

--- django_chatbot/chatbot/views.py
+++ django_chatbot/chatbot/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import openai
+
+openai_api_key = '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'
+openai.api_key = openai_api_key
+def ask_openai(message):
+    response=openai.Completion.create(
+        model="1"
+    )
+
 
 # Create your views here.
 def chatbot(request):
-    return render(request,'chatbot.html')
+    
+
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = 'hi hhh'
+        return JsonResponse({'message': message, 'response': response})
+    return render(request, 'chatbot.html')
      
